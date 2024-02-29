0161f746d23942cab11fd1a428e0ba981a42e19d
JohnRofe <110251263+JohnRofe@users.noreply.github.com>
2024-02-29 14:46:13 -0600

    refactor function

--- main.py
+++ main.py
@@ -13,21 +13,19 @@ def process_title(title):
     brand, model, year = rest.split('.')
     return brand, model, year
 
+def extract_number(text):
+    match = re.search(r'\d+', text)
+    return match.group() if match else None
+
 def extract_car_data(car):
-    title = car.css_first('td.brandtitle-sm > a').attrs['href']
-    brand, model, year = process_title(title)
-
-    passengers_text = car.css_first('td.brandtitle-sm > span').text().strip()
-    passengers_match = re.search(r'\d+', passengers_text)
-    passengers = passengers_match.group() if passengers_match else None
-
-    price_text = car.css_first('span.precio-sm').text().strip()
-    price_match = re.search(r'\d+', price_text.replace(',', ''))
-    price = int(price_match.group()) if price_match else None
-
-    details = car.css_first('div.transtitle').text().replace('|', '').strip()
-    details_list = [item.strip() for item in details.split('\n') if item.strip()]
-    return {'brand': brand, 'model': model, 'year': year, 'passangers': passengers, 'price': price, 'details': details_list}
+    data = {
+        'title': car.css_first('td.brandtitle-sm > a').attrs['href'],
+        'passengers': extract_number(car.css_first('td.brandtitle-sm > span').text().strip()),
+        'price': int(extract_number(car.css_first('span.precio-sm').text().replace(',', ''))),
+        'details': [item.strip() for item in car.css_first('div.transtitle').text().replace('|', '').strip().split('\n') if item.strip()]
+    }
+    data['brand'], data['model'], data['year'] = process_title(data['title'])
+    return data
 
 if __name__ == "__main__":
 
@@ -36,9 +34,10 @@ if __name__ == "__main__":
     html = scraper.get_html()
     cars = get_data_container(html)
     data = [extract_car_data(car) for car in cars]
+    with open('cars.json', 'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
     scraper.close()
-    with open('cars.json', 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
+
 
 
 
